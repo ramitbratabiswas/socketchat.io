46ce8fe8b685b3cf16900f03fc1a114b98fa79c2
Ramit Brata Biswas <ramitbratabiswas@gmail.com>
2024-07-12 10:22:37 -0700

    added server

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import path, { dirname } from "path";
+import http from "http";
+import { Server } from "socket.io";
+
+dotenv.config();
 
 const app = express();
+app.use(express.static(path.join(import.meta.dirname, 'public')));
 
-app.use(express.static("public"));
-app.listen(8080, () => {
-  console.log("listening on port 8080");
-});
+const server = http.createServer(app);
+const io = new Server(server);
 
-app.get("/yay", (req, res) => {
-  res.json({ "yay" : "yayyy"});
+io.on('connection', socket => {
+  console.log('new websocket connection...');
 })
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
