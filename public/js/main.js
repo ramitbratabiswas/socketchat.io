e8d3d4e704692e48b24313b186e2a62c1c50dc4a
Ramit Brata Biswas <ramitbratabiswas@gmail.com>
2024-07-13 21:45:45 -0700

    added error checks

--- public/js/main.js
+++ public/js/main.js
@@ -72,3 +72,14 @@ const outputUsers = ( users ) => {
 }
 
 console.log(socket);
+
+socket.on("connect_error", (err) => {
+  // the reason of the error, for example "xhr poll error"
+  console.log(err.message);
+
+  // some additional description, for example the status code of the initial HTTP response
+  console.log(err.description);
+
+  // some additional context, for example the XMLHttpRequest object
+  console.log(err.context);
+});
