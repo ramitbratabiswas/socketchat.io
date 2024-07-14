bd936e08b35e195c2fdb346c65951a5d37d5d5b9
Ramit Brata Biswas <ramitbratabiswas@gmail.com>
2024-07-13 22:34:16 -0700

    giving up

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,7 @@
 import { io } from "https://cdn.socket.io/4.7.5/socket.io.esm.min.js";
   
 const socket = io({
-  transports: ['websocket']
+  transports: ['long-polling','websocket']
 });
 console.log(socket);
 
